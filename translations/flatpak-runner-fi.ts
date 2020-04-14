88919a7b5dc2cc136194f52816a6fe718abc77c8
Rinigus <rinigus.git@gmail.com>
2020-04-14 20:52:37 +0300

    add Finnish translation

---
+++ translations/flatpak-runner-fi.ts
@@ -0,0 +1,236 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fi" version="2.1">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="55"/>
+        <source>About Flatpak Runner</source>
+        <translation>Tietoja Flatpak Runnerista</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="67"/>
+        <source>Version</source>
+        <translation>Versio</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="71"/>
+        <source>License: BSD-3</source>
+        <translation>Lisenssi: BSD-3</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="75"/>
+        <source>Homepage: &lt;a href=&apos;https://github.com/sailfishos-flatpak/flatpak-runner&apos;&gt;https://github.com/sailfishos-flatpak/flatpak-runner&lt;/a&gt;</source>
+        <translation>Kotisivu: &lt;a href=&apos;https://github.com/sailfishos-flatpak/flatpak-runner&apos;&gt;https://github.com/sailfishos-flatpak/flatpak-runner&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/AboutPage.qml" line="79"/>
+        <source>This application allows you to refresh list of installed Flatpak applications, set environment variables applied by default to all applications and the application-specific ones. In addition, it is used to host Flatpak applications while they run on Sailfish OS.</source>
+        <translation>Tämän työkalun avulla voit päivittää listan asennetuista Flatpak-sovelluksista ja asettaa ympäristömuuttujia sekä sovelluskohtaisesti että kaikille sovelluksille yhteisesti. Lisäksi se toimii Flatpak-sovellusten isäntänä Sailfish OS -alustalla.</translation>
+    </message>
+</context>
+<context>
+    <name>AppSettingsPage</name>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="63"/>
+        <source>Apply</source>
+        <translation>Käytä</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="64"/>
+        <source>Settings: %1</source>
+        <translation>Asetukset: %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="74"/>
+        <source>Override theme handling</source>
+        <translation>Pakota teeman käsittely</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="75"/>
+        <source>Override default theme handling.</source>
+        <translation>Pakota oletusteeman käsittely.</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="81"/>
+        <source>Theme selection</source>
+        <translation>Teeman valinta</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="82"/>
+        <source>Set application theme.</source>
+        <translation>Aseta sovelluksen teema</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="84"/>
+        <source>Automatic</source>
+        <translation>Automaattinen</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="85"/>
+        <source>Light</source>
+        <translation>Vaalea</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="86"/>
+        <source>Dark</source>
+        <translation>Tumma</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="87"/>
+        <source>Manual</source>
+        <translation>Manuaalinen</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="133"/>
+        <source>Override scaling factor</source>
+        <translation>Pakota skaalauskerroin</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="134"/>
+        <source>Usually, it is not needed to override scaling factor. However, applications using QtWebEngine may require overriding the factor</source>
+        <translation>Yleensä skaalauskerrointa ei ole tarve muuttaa, mutta jotkut sovellukset, jotka käyttävät komponenttia QtWebEngine, saattavat vaatia tämän asetuksen käyttöä piirtyäkseen oikein.</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="178"/>
+        <source>Override DPI</source>
+        <translation>Pakota DPI-arvo</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="179"/>
+        <source>DPI is detected by default. When overriding scaling factor, new DPI will be calculated by reducing it accordingly. If, for some reason, you need still to force DPI, you could override it here</source>
+        <translation>Laitteen DPI-arvo tunnistetaan automaattisesti. Kun asetat käsin skaalauskertoimen, uusi DPI-arvo lasketaan sen pohjalta automaattisesti. Jos jostakin syystä haluat muuttaa DPI-arvoa, voit tehdä sen tässä.</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="225"/>
+        <source>Override keyboard handling</source>
+        <translation>Pakota näppäimistön käsittely</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="226"/>
+        <source>Override default keyboard handling.</source>
+        <translation>Pakota oletusnäppäimistön käsittely.</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="232"/>
+        <source>Reduce window when keyboard is shown</source>
+        <translation>Pienennä ikkunaa kun näppäimistö on auki</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="233"/>
+        <source>When enabled, surface showing the application is reduced to ensure that the keyboard does not cover the application parts. This is sometimes needed in some of the applications. However, it may cause temporary distortion in the shown window due to rescaling of the window before the application catches up and redraws the window.</source>
+        <translation>Kun asetus on päällä, sovelluksen ikkunan kokoa pienennetään, jolloin näppäimistö ei peitä ikkunan sisältöä alleen. Tämä on tarpeen joidenkin sovellusten kohdalla. Näppäimistön sulkeminen ja avaaminen saattaa aiheuttaa hetkellisiä graafisia häiriöitä, kunnes sovellus on ehtinyt piirtää sisältönsä kokonaan uudelleen eri kokoiseen ikkunaan.</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="293"/>
+        <source>Environment variable</source>
+        <translation>Ympäristömuuttuja</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="299"/>
+        <source>Name</source>
+        <translation>Nimi</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="307"/>
+        <source>Value</source>
+        <translation>Arvo</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="316"/>
+        <source>Delete</source>
+        <translation>Poista</translation>
+    </message>
+    <message>
+        <location filename="../qml/AppSettingsPage.qml" line="348"/>
+        <source>Add new variable</source>
+        <translation>Lisää ympäristömuuttuja</translation>
+    </message>
+</context>
+<context>
+    <name>ExtensionPage</name>
+    <message>
+        <location filename="../qml/ExtensionPage.qml" line="62"/>
+        <source>Flatpak Extension</source>
+        <translation>Flatpak-laajennus</translation>
+    </message>
+    <message>
+        <location filename="../qml/ExtensionPage.qml" line="66"/>
+        <source>Here you can refresh your Flatpak extension or remove it.</source>
+        <translation>Tässä voit päivittää Flatpak-laajennuksen tai poistaa sen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/ExtensionPage.qml" line="70"/>
+        <source>Flatpak extension allows to use graphics on devices that are not supported by Flatpak runtime. For example, libhybris-based devices.</source>
+        <translation>Flatpak-laajennus mahdollistaa graafiset sovellukset laitteilla, joita Flatpak-järjestelmä ei tue - esimerkkinä libhybris-pohjaiset laitteet.</translation>
+    </message>
+    <message>
+        <location filename="../qml/ExtensionPage.qml" line="75"/>
+        <location filename="../qml/ExtensionPage.qml" line="87"/>
+        <source>Update extension</source>
+        <translation>Päivitä laajennus</translation>
+    </message>
+    <message>
+        <location filename="../qml/ExtensionPage.qml" line="79"/>
+        <source>Update extension to reflect currently installed libhybris and support libraries. This is required after update of Sailfish OS.</source>
+        <translation>Päivitä laajennus vastaamaan nykyistä libhybris-asennusta ja kirjastoja. Tämä on tehtävä Sailfish OS -järjestelmäpäivityksen jälkeen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/ExtensionPage.qml" line="97"/>
+        <location filename="../qml/ExtensionPage.qml" line="109"/>
+        <source>Remove extension</source>
+        <translation>Poista laajennus</translation>
+    </message>
+    <message>
+        <location filename="../qml/ExtensionPage.qml" line="101"/>
+        <source>Remove extension from your home folder. Note that it will be created again if you start Flatpak Runner.</source>
+        <translation>Poista laajennus kotihakemistostasi. Se luodaan automaattisesti uudelleen kun käynnistät Flatpak Runnerin.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/MainPage.qml" line="73"/>
+        <source>Flatpak Runner</source>
+        <translation>Flatpak Runner</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="130"/>
+        <source>About</source>
+        <translation>Tietoja</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="134"/>
+        <source>Extension</source>
+        <translation>Laajennus</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="147"/>
+        <source>Default settings</source>
+        <translation>Oletusasetukset</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="199"/>
+        <source>Application crashed</source>
+        <translation>Sovellus kaatui</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="202"/>
+        <source>Application finished with exit code %1</source>
+        <translation>Sovelluksen suoritus päättyi arvoon %1</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="204"/>
+        <source>Application finished</source>
+        <translation>Sovellus sulkeutui</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="234"/>
+        <source>Update list of applications</source>
+        <translation>Päivitä sovelluslista</translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="243"/>
+        <source>Initialize or update GL extension</source>
+        <translation>Luo tai päivitä GL-laajennus</translation>
+    </message>
+</context>
+</TS>
